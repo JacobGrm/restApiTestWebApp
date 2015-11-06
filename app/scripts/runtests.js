cea5fb66f9f800f45a2f14744c1cfc0894a43d0a
Grimberg, Jacob (GE Global Research) <Jacob.Grimberg@ge.com>
2015-11-05 17:15:26 -0800

    Moved test code to different location

--- app/scripts/runtests.js
+++ app/scripts/runtests.js
@@ -3,29 +3,34 @@ function randomNumber(min, max) {
 }
 
 var docName = "command_multi_" + randomNumber(100,1000);
-function test() {
+var  testResult = '';
+
+function test1() {
+
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
+
         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+
             window.response = xmlhttp.responseText;
             window.status = xmlhttp.status;
-            if(xmlhttp.status >= 200 && xmlhttp.status < 300){
+            if(xmlhttp.status >= 200 && xmlhttp.status < 300) {
                 if (xmlhttp.responseText.indexOf("true") !=-1)
                 {
-                    window.testResult = "PASS";
+                    testResult = "PASS";
                 }
                 else
                 {
-                    window.testResult = "FAIL";
+                    testResult = "FAIL";
                 }
             }
             else
             {
-                window.testResult = "FAIL";
+                testResult = "FAIL";
             }
-            alert(window.testResult);
         }
     };
+
     var data = {
         "type": "command",
         "~userid": "user1",
@@ -42,7 +47,7 @@ function test() {
         }
     };
 
-    xmlhttp.open("PUT", "http://pmapi/cdb/predixgo/" + docName, true);
+    xmlhttp.open("PUT", "http://pmapi/cdb/predixgo/" + docName, false);
     xmlhttp.setRequestHeader("Content-type","application/json;charset=UTF-8");
     xmlhttp.setRequestHeader("Content-length", data.length);
     xmlhttp.send(JSON.stringify(data));
@@ -50,6 +55,7 @@ function test() {
 }
 
 function test2() {
+
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
@@ -57,22 +63,41 @@ function test2() {
             if(xmlhttp.status == 200){
                 if (xmlhttp.responseText.indexOf(docName) !=-1)
                 {
-                    window.testResult = "PASS";
+                    testResult = "PASS";
                 }
                 else
                 {
-                    window.testResult = "FAIL";
+                    testResult = "FAIL";
                 }
             }
             else
             {
-                window.testResult = "FAIL";
+                testResult = "FAIL";
             }
-            alert(window.testResult + ': ' + window.response);
         }
     };
 
-    xmlhttp.open("GET", "http://pmapi/cdb/predixgo/" + docName, true);
-
+    xmlhttp.open("GET", "http://pmapi/cdb/predixgo/" + docName, false);
     xmlhttp.send();
 }
+
+
+describe('PredixMobile API Tests', function() {
+
+    describe('Create and process a command', function() {
+
+        it("Create a command by posting to the local CouchDb", function () {
+
+            test1();
+            expect(testResult).toEqual('PASS');
+        });
+
+        it("Verify command synced to the backend, processed and sycned back to the client", function () {
+
+            test2();
+            expect(testResult).toEqual('PASS');
+        });
+
+    });
+
+});
