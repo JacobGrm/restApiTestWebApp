a38130e1942eb58a3eeed90686ad423ad328a809
Grimberg, Jacob (GE Global Research) <Jacob.Grimberg@ge.com>
2015-11-13 17:32:05 -0800

    updated

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-# myproj
-This repository contains the web components for myproj.
-# restApiTestWebApp
+
+Prerequisites:
+
+- Create boot document for the test user
+
+{
+    "_id": "boot-document-test-app",
+    "_rev": "19-bf6e4e862cedb8bd259a4f18f8264257",
+    "authType": "",
+    "bundleName": "",
+    "channels": [
+    "entity_Jacob_Grimberg_ge_com"
+    ],
+    "created_at": 1446851799498,
+    "offlinePkgName": "",
+    "pkgName": "data-app-px-end2end-test-app-2",
+    "startTS": 1466851799498,
+    "type": "boot",
+    "updated_at": 1446851799498
+}
+
+ Put "data-app-px-end2end-test-app-2" for "pgName":
+
+
+1. Check out project from git repo
+
+2. cd to 'px-end2end-test-web-app'
+
+3. Run 'pg doctor' to make sure connection is OK
+
+4. Run 'gulp dist' to build dist folder
+
+5. Run 'pg publish' to build .zip file and push it to couchDb
+
+6. Start app on a mobile device
+
+7. Click "Run Test" button to start executing tests
+
+
+If you want to modify the app (add more tests)
+
+1. Add new test to index.jade
+
+        script(type='text/javascript', src='scripts/myNewTest.js')
+
+2. gulp clean
+
+3. gulp dist
+
+4. pg publish
+
+
+
+
--- app/spec/helper.js
+++ app/spec/helper.js
@@ -1,5 +1,12 @@
+var g_httpResponse;
+var g_httpStatus;
+var g_xmlhttp;
+
+
 afterEach(function() {
-    document.getElementById('stage').innerHTML = '';
+
+    if(typeof g_xmlhttp != 'undefined')
+        g_xmlhttp.abort();
 });
 
 var helper = {
@@ -9,3 +16,93 @@ var helper = {
         obj.dispatchEvent(e);
     }
 };
+
+
+function randomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
+function generateUniqueId(){
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+        .replace(/[xy]/g, function(c) {
+            var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return v.toString(16);
+        });
+};
+
+function sleep(dur) {
+    var d = new Date().getTime() + dur;
+    while(new Date().getTime() <= d ) {
+        //Do nothing
+    }
+
+};
+
+function stripQuotes(str) {
+
+    return str.replace(/^"(.*)"$/, '$1');
+
+};
+
+function generateJSON_UniqueKeys(numberOfKeys) {
+
+    jsonObj = {};
+
+    for(var i=0; i<numberOfKeys; i++) {
+
+        jsonObj["key_"+randomNumber(100,1000)]="value_"+i;
+    }
+
+    return jsonObj;
+
+};
+
+function isJson(obj) {
+    var t = typeof obj;
+    return ['boolean', 'number', 'string', 'symbol', 'function'].indexOf(t) == -1;
+};
+
+/*
+    API calls
+
+ */
+
+function _setUpHttpRequest(options) {
+
+    g_xmlhttp = new XMLHttpRequest();
+    g_xmlhttp.onreadystatechange = function() {
+
+        if (g_xmlhttp.readyState == XMLHttpRequest.DONE ) {
+
+            g_httpResponse = g_xmlhttp.responseText;
+            g_httpStatus = g_xmlhttp.status;
+        }
+    };
+
+    g_xmlhttp.open(options.method, options.url, false);
+
+
+    if(options.json === true) {
+
+        g_xmlhttp.setRequestHeader("Content-type","application/json;charset=UTF-8");
+        g_xmlhttp.setRequestHeader("Content-length", options.data.length);
+        g_xmlhttp.send(JSON.stringify(options.data));
+
+    } else if (options.json === false) {
+
+        g_xmlhttp.setRequestHeader("Content-type","application/text;charset=UTF-8");
+        g_xmlhttp.setRequestHeader("Content-length", options.data.length);
+        g_xmlhttp.send(JSON.stringify(options.data));
+
+
+    } else if (typeof  options.json === 'undefined') {
+
+        g_xmlhttp.send();
+    }
+
+}
+
+
+
